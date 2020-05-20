cec2110c4731051fdcff2f4d053ad85367e8ee75
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-20 09:03:14 +0300

    added department table

---
+++ migrations/m200520_060137_create_department_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%department}}`.
+ */
+class m200520_060137_create_department_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%department}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(255)->notNull(),
+            'description' => $this->text()
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%department}}');
+    }
+}
