012edb6811a324d53e64902bf86e97a5d8b243aa
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-21 20:24:45 +0300

    edited queries.sql

--- db_queries/queries.sql
+++ db_queries/queries.sql
@@ -4,13 +4,16 @@ FROM `group`
          INNER JOIN `academic_plan` ON `student`.`academic_plan_id` = `academic_plan`.`id`
          INNER JOIN `subject_academic_plan` ON `subject_academic_plan`.`academic_plan_id` = `academic_plan`.`id`
          INNER JOIN `subject` ON `subject_academic_plan`.`subject_id` = `subject`.`id`
-         INNER JOIN `mark` ON `mark`.`subject_id` = `subject`.`id`
+         INNER JOIN `mark` ON `mark`.`subject_id` = `subject`.`id` AND `mark`.`value` IS NOT NULL
 GROUP BY `group`.`name`, `subject`.`name`;
 
 --
-SET @beginning = '';
-SET @ending = '';
-SELECT `group`.`name`, `student`.`name`, `student`.`surname`, `student`.`patronymic`
+SET @beginning = CAST("2017-08-29 00:00:00" AS DATETIME);
+SET @ending = CAST("2020-08-29 00:00:00" AS DATETIME);
+SELECT `group`.`name`,
+       `student`.`name`,
+       `student`.`surname`,
+       `student`.`patronymic`
 FROM `subject`
          INNER JOIN `subject_academic_plan` ON `subject`.`id` = `subject_academic_plan`.`subject_id`
          INNER JOIN `academic_plan` ON `academic_plan`.`id` = `subject_academic_plan`.`academic_plan_id`
@@ -18,33 +21,40 @@ FROM `subject`
          INNER JOIN `group` ON `group`.`id` = `student`.`group_id`
          INNER JOIN `mark` ON `mark`.`student_id` = `student`.`id` AND `mark`.`valuation_date` >= @beginning AND
                               `mark`.`valuation_date` <= @ending
-WHERE `mark`.`value` = 0
-   OR `mark`.`value` = 2;
--- TODO добавить границы
+WHERE `mark`.`absent` = 1
+   OR `mark`.`value` = 2
+GROUP BY `group`.`name`,
+         `student`.`name`,
+         `student`.`surname`,
+         `student`.`patronymic`;
 
 --
 
 -- SELECT LEFT('abffagpokejfkjs', LENGTH('hello'))
-SET @name_pattern = '';
-SET @surname_pattern = '';
-SET @patronymic_pattern = '';
+SET @name_pattern = 'Э';
+SET @surname_pattern = 'М';
+SET @patronymic_pattern = 'И';
 SELECT *
-FROM student
-WHERE LEFT(student.name, LENGTH(@name_pattern)) = @name_pattern
-  AND LEFT(student.surname, LENGTH(@surname_pattern)) = @surname_pattern
-  AND LEFT(student.patronymic, LENGTH(@patronymic_pattern)) = @patronymic_pattern;
+FROM `student`
+WHERE `student`.`name` LIKE CONCAT(@name_pattern, '%')
+  AND `student`.`surname` LIKE CONCAT(@surname_pattern, '%')
+  AND `student`.`patronymic` LIKE CONCAT(@patronymic_pattern, '%');
 
 
 --
 
-SELECT `student`.`name` as `Имя студента`
+SELECT `student`.`name` as `Имя студента`,
+       `student`.`surname` as `Фамилия студента`,
+       `student`.`patronymic` as `Отчёство студента`,
+       AVG(`mark`.`value`) as `Средняя оценка`,
+       (COUNT(`mark`.`absent`) / COUNT(*)) as `Пропуски`
 FROM `group`
          INNER JOIN `student` ON `group`.`id` = `student`.`group_id`
          INNER JOIN `academic_plan` ON `student`.`academic_plan_id` = `academic_plan`.`id`
          INNER JOIN `subject_academic_plan` ON `subject_academic_plan`.`academic_plan_id` = `academic_plan`.`id`
          INNER JOIN `subject` ON `subject_academic_plan`.`subject_id` = `subject`.`id`
          INNER JOIN `mark` ON `mark`.`subject_id` = `subject`.`id`
-GROUP BY `student`.`name`
+GROUP BY `student`.`name`, `student`.`surname`, `student`.`patronymic`
 HAVING AVG(`mark`.`value`) > 2.75
-   AND (COUNT(`mark`.`absent`) / COUNT(*)) > 0.3
+   AND (COUNT(`mark`.`absent`) / COUNT(*)) > 0.3;
 -- TODO пропущенные занятия
