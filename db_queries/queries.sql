de59b3fe85586053b66b9ff0dbae92ef27cb9f7c
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-22 10:22:40 +0300

    edited queries.sql

--- db_queries/queries.sql
+++ db_queries/queries.sql
@@ -43,11 +43,11 @@ WHERE `student`.`name` LIKE CONCAT(@name_pattern, '%')
 
 --
 
-SELECT `student`.`name` as `Имя студента`,
-       `student`.`surname` as `Фамилия студента`,
-       `student`.`patronymic` as `Отчёство студента`,
-       AVG(`mark`.`value`) as `Средняя оценка`,
-       (COUNT(`mark`.`absent`) / COUNT(*)) as `Пропуски`
+SELECT `student`.`name`                                                              as `Имя студента`,
+       `student`.`surname`                                                           as `Фамилия студента`,
+       `student`.`patronymic`                                                        as `Отчёство студента`,
+       AVG(`mark`.`value`)                                                           as `Средняя оценка`,
+       (COUNT(CASE WHEN `mark`.`absent` >= 1 THEN 1 ELSE NULL END) / COUNT(*)) * 100 as `Процент пропусков`
 FROM `group`
          INNER JOIN `student` ON `group`.`id` = `student`.`group_id`
          INNER JOIN `academic_plan` ON `student`.`academic_plan_id` = `academic_plan`.`id`
@@ -56,5 +56,46 @@ FROM `group`
          INNER JOIN `mark` ON `mark`.`subject_id` = `subject`.`id`
 GROUP BY `student`.`name`, `student`.`surname`, `student`.`patronymic`
 HAVING AVG(`mark`.`value`) > 2.75
-   AND (COUNT(`mark`.`absent`) / COUNT(*)) > 0.3;
--- TODO пропущенные занятия
+   AND (COUNT(CASE WHEN `mark`.`absent` >= 1 THEN 1 ELSE NULL END) / COUNT(*)) > 0.3;
+
+--
+
+SET @target_group_id = 1;
+
+UPDATE `student`
+SET `student`.`group_id` = ()
+WHERE `student`.`id` in (SELECT `student`.`id` as `id`
+                         FROM `group`
+                                  INNER JOIN `student`
+                                             ON `group`.`id` = @target_group_id AND `group`.`id` = `student`.`group_id`
+                                  INNER JOIN `academic_plan` ON `student`.`academic_plan_id` = `academic_plan`.`id`
+                                  INNER JOIN `subject_academic_plan`
+                                             ON `subject_academic_plan`.`academic_plan_id` = `academic_plan`.`id`
+                                  INNER JOIN `subject` ON `subject_academic_plan`.`subject_id` = `subject`.`id`
+                                  INNER JOIN `mark` ON `mark`.`subject_id` = `subject`.`id`
+                         GROUP BY `student`.`id`
+                         HAVING AVG(`mark`.`value`) <= 2.75
+                             OR (COUNT(CASE WHEN `mark`.`absent` >= 1 THEN 1 ELSE NULL END) / COUNT(*)) <= 0.3);
+UPDATE `group`
+SET `group`.`course_number` = `group`.`course_number` + 1
+WHERE `group`.`id` = @target_group_id;
+--
+SET @target_group_id = 1;
+SET @target_group_course_number = (SELECT `course_number`
+                     FROM `group`
+                     WHERE `group`.`id` = @target_group_id);
+SET @target_group_specialty_id = (SELECT `specialty_id`
+                     FROM `group`
+                     WHERE `group`.`id` = @target_group_id);
+SET @new_group_id = (SELECT `group`.`id`
+                     FROM `group`
+                     WHERE `group`.`course_number` = @target_group_course_number
+                       AND `group`.`id` != @target_group_id
+                     LIMIT 1);
+IF COUNT(@new_group_id) = 0 THEN
+INSERT `group`(`name`, `course_number`, `specialty_id`)
+VALUES (LEFT(MD5(RAND()), 8), @target_group_course_number, @target_group_specialty_id);
+SELECT LAST_INSERT_ID();
+ELSE
+SELECT @new_group_id;
+END IF;
