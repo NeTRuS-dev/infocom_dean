c710106c1f34e70469eae9a6481b82c21893ba51
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-22 20:40:34 +0300

    displaying students report

--- views/site/students-displayer.php
+++ views/site/students-displayer.php
@@ -1,3 +1,42 @@
 <?php
 /* @var $data_containers \app\models\DataContainer[] */
+
+use app\models\DataContainer;
+use yii\bootstrap4\LinkPager;
+use yii\data\ArrayDataProvider;
+use yii\grid\GridView;
+
 /* @var $this yii\web\View */
+/**
+ * @param \app\models\DataContainer $container
+ * @param int $index
+ * @throws Exception
+ */
+function display_data_container(DataContainer $container, int $index)
+{
+    $data_provider = new ArrayDataProvider([
+            'allModels' => $container->main_data,
+            'pagination' => [
+                'pageSize' => 10,
+                'pageParam' => $index . '-data-page',
+            ],
+            'sort' => [
+                'sortParam' => $index . '-data-sort'
+            ]
+        ]
+    );
+    echo GridView::widget([
+        'summary' => '',
+        'options' => ['class' => 'grid-view w-50'],
+        'headerRowOptions' => ['class' => 'text-center'],
+        'dataProvider' => $data_provider,
+        'pager' => [
+            'class' => LinkPager::class
+        ]
+
+    ]);
+}
+
+
+
+
