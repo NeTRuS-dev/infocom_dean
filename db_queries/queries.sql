d2e139f196cde2e7572bedda48c91dfcb09115f6
NeTRuS-dev <vlad-broxxx@yandex.ru>
2020-05-22 11:30:54 +0300

    edited queries.sql

--- db_queries/queries.sql
+++ db_queries/queries.sql
@@ -59,43 +59,68 @@ HAVING AVG(`mark`.`value`) > 2.75
    AND (COUNT(CASE WHEN `mark`.`absent` >= 1 THEN 1 ELSE NULL END) / COUNT(*)) > 0.3;
 
 --
+DELIMITER //
 
-SET @target_group_id = 1;
-
-UPDATE `student`
-SET `student`.`group_id` = ()
-WHERE `student`.`id` in (SELECT `student`.`id` as `id`
+CREATE PROCEDURE UpCourseForGroupMembers(
+    IN target_group_id INT
+)
+BEGIN
+    SET @target_group_course_number = (SELECT `course_number`
+                                       FROM `group`
+                                       WHERE `group`.`id` = target_group_id);
+    SET @target_group_specialty_id = (SELECT `specialty_id`
+                                      FROM `group`
+                                      WHERE `group`.`id` = target_group_id);
+    SET @new_group_id = (SELECT `group`.`id`
                          FROM `group`
-                                  INNER JOIN `student`
-                                             ON `group`.`id` = @target_group_id AND `group`.`id` = `student`.`group_id`
-                                  INNER JOIN `academic_plan` ON `student`.`academic_plan_id` = `academic_plan`.`id`
-                                  INNER JOIN `subject_academic_plan`
-                                             ON `subject_academic_plan`.`academic_plan_id` = `academic_plan`.`id`
-                                  INNER JOIN `subject` ON `subject_academic_plan`.`subject_id` = `subject`.`id`
-                                  INNER JOIN `mark` ON `mark`.`subject_id` = `subject`.`id`
-                         GROUP BY `student`.`id`
-                         HAVING AVG(`mark`.`value`) <= 2.75
-                             OR (COUNT(CASE WHEN `mark`.`absent` >= 1 THEN 1 ELSE NULL END) / COUNT(*)) <= 0.3);
-UPDATE `group`
-SET `group`.`course_number` = `group`.`course_number` + 1
-WHERE `group`.`id` = @target_group_id;
+                         WHERE `group`.`course_number` = @target_group_course_number
+                           AND `group`.`id` != target_group_id
+                         LIMIT 1);
+    IF ISNULL(@new_group_id) THEN
+        INSERT `group`(`name`, `course_number`, `specialty_id`)
+        VALUES (LEFT(MD5(RAND()), 7), @target_group_course_number, @target_group_specialty_id);
+        SET @new_group_id = LAST_INSERT_ID();
+    END IF;
+
+    UPDATE `student`
+    SET `student`.`group_id` = @new_group_id
+    WHERE `student`.`id` in (SELECT `student`.`id` as `id`
+                             FROM `group`
+                                      INNER JOIN `student`
+                                                 ON `group`.`id` = target_group_id AND `group`.`id` = `student`.`group_id`
+                                      INNER JOIN `academic_plan` ON `student`.`academic_plan_id` = `academic_plan`.`id`
+                                      INNER JOIN `subject_academic_plan`
+                                                 ON `subject_academic_plan`.`academic_plan_id` = `academic_plan`.`id`
+                                      INNER JOIN `subject` ON `subject_academic_plan`.`subject_id` = `subject`.`id`
+                                      INNER JOIN `mark` ON `mark`.`subject_id` = `subject`.`id`
+                             GROUP BY `student`.`id`
+                             HAVING AVG(`mark`.`value`) <= 2.75
+                                 OR (COUNT(CASE WHEN `mark`.`absent` >= 1 THEN 1 ELSE NULL END) / COUNT(*)) <= 0.3);
+
+    UPDATE `group`
+    SET `group`.`course_number` = `group`.`course_number` + 1
+    WHERE `group`.`id` = target_group_id;
+END //
+
+DELIMITER ;
+
 --
-SET @target_group_id = 1;
-SET @target_group_course_number = (SELECT `course_number`
-                     FROM `group`
-                     WHERE `group`.`id` = @target_group_id);
-SET @target_group_specialty_id = (SELECT `specialty_id`
-                     FROM `group`
-                     WHERE `group`.`id` = @target_group_id);
-SET @new_group_id = (SELECT `group`.`id`
-                     FROM `group`
-                     WHERE `group`.`course_number` = @target_group_course_number
-                       AND `group`.`id` != @target_group_id
-                     LIMIT 1);
-IF COUNT(@new_group_id) = 0 THEN
-INSERT `group`(`name`, `course_number`, `specialty_id`)
-VALUES (LEFT(MD5(RAND()), 8), @target_group_course_number, @target_group_specialty_id);
-SELECT LAST_INSERT_ID();
-ELSE
-SELECT @new_group_id;
-END IF;
+# SET @target_group_id = 1;
+# SET @target_group_course_number = (SELECT `course_number`
+#                                    FROM `group`
+#                                    WHERE `group`.`id` = @target_group_id);
+# SET @target_group_specialty_id = (SELECT `specialty_id`
+#                                   FROM `group`
+#                                   WHERE `group`.`id` = @target_group_id);
+# SET @new_group_id = (SELECT `group`.`id`
+#                      FROM `group`
+#                      WHERE `group`.`course_number` = @target_group_course_number
+#                        AND `group`.`id` != @target_group_id
+#                      LIMIT 1);
+# IF COUNT(@new_group_id) = 0 THEN
+# INSERT `group`(`name`, `course_number`, `specialty_id`)
+# VALUES (LEFT(MD5(RAND()), 8), @target_group_course_number, @target_group_specialty_id);
+# SELECT LAST_INSERT_ID();
+# ELSE
+# SELECT @new_group_id;
+# END IF;
